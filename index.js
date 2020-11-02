950009cc43b990cac0c5a137f4867d058bcfeb38
cerulean-revision-6689 <jordanjennings12@gmail.com>
2020-11-02 23:33:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,17 @@ function logWhisper(string){
   var x = string.toLowerCase()
   console.log(x)
 }
+
+function sayHiToGrandma(string){
+  var uppercase = "HELLO"
+  if(uppercase.toUpperCase()===uppercase)
+  return "YES INDEED!"
+
+  var lowercase = "hello"
+  if(lowercase.toLowerCase()===lowercase)
+  return "I can/t hear you!"
+
+  var love = "I love you, Grandma."
+  if(string === love)
+  return "I love you, too."
+}
