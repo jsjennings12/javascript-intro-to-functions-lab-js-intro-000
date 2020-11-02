a7777cb9c4f5960ec7bbe6c9c279dfb39d87a2ed
cerulean-revision-6689 <jordanjennings12@gmail.com>
2020-11-02 23:17:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  var x = string.toUpperCase()
+  console.log(x)
+}
+
+function logWhisper(string){
+  var x = string.toLowerCase()
+  console.log(x)
+}
