b89c052f514c91af95f4bc209967cd526af7f1f4
cerulean-revision-6689 <jordanjennings12@gmail.com>
2020-11-02 23:40:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,14 +18,15 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   var lowercase = "hello"
-  if(lowercase.toLowerCase()===lowercase)
+  if(lowercase.toLowerCase()===lowercase){
   return "I can/t hear you!"
-
+}
   var uppercase = "HELLO"
-  if(uppercase.toUpperCase()===uppercase)
+  if else(uppercase.toUpperCase()===uppercase){
   return "YES INDEED!"
-
+}
   var love = "I love you, Grandma."
-  if(string === love)
+  if(string === love){
   return "I love you, too."
+}
 }
